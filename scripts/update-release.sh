6f8fb8465cc9f5d76bf98b21b2faee9b2569636b
Hayato Tokuoka <t-hatokuoka@zlab.co.jp>
2017-11-06 18:10:37 +0900

    update text

--- scripts/update-release.sh
+++ scripts/update-release.sh
@@ -26,4 +26,4 @@ github-release edit \
     --repo ${GITHUB_REPO} \
     --tag  ${TAG} \
     --name ${TAG} \
-    --description "## Change List\r\n\r\n- Update ansible-infra-common v1.21.0 #14\r\n\r\n## Artifacts\r\n\r\n\r\n| イメージ名 |\r\n| --- |\r\n| ${IMAGE_NAME} |"
+    --description "## Change List\\r\\n\\r\\n- Update ansible-infra-common v1.21.0 #14\\r\\n\\r\\n## Artifacts\\r\\n\\r\\n\\r\\n| イメージ名 |\\r\\n| --- |\\r\\n| ${IMAGE_NAME} |"
