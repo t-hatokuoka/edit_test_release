778b535a4ef2aed0039cb63012f25985496f2400
Hayato Tokuoka <t-hatokuoka@zlab.co.jp>
2017-11-07 11:48:42 +0900

    update text6

--- scripts/update-release.sh
+++ scripts/update-release.sh
@@ -26,7 +26,7 @@ github-release edit \
     --repo ${GITHUB_REPO} \
     --tag  ${TAG} \
     --name ${TAG} \
-    --description @<(cat EOT
+    --description @<(cat <<EOT
 
 - Update ansible-infra-common v1.21.0
 
